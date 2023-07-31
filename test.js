fe196bff8b1bea4d443bf20e8ac9ae0a33da42cc
AlanRoybal <MysonRoybal@gmail.com>
2023-07-30 19:48:13 -0500

    changed test.js to fetch and removed words txt file

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
+const axios = require('axios');
 const fs = require('fs');
 
-function loadDictionary(filePath) {
+function loadDictionary(data) {
   try {
-    const data = fs.readFileSync(filePath, 'utf8');
     return data.split('\n').map(word => word.trim().toLowerCase());
   } catch (error) {
     console.error('Error loading dictionary:', error.message);
@@ -10,34 +10,55 @@ function loadDictionary(filePath) {
   }
 }
 
-const dictionaryFilePath = 'words_alpha.txt.txt'; //english word dictionary
-const dictionary = loadDictionary(dictionaryFilePath);
-
-function isWordValid(word) {
-  return dictionary.includes(word.toLowerCase());
-}
-
-function spellCheck(text) {
-  const words = text.split(/\s+/);
-  const result = [];
-
-  for (const word of words) {
-    const trimmedWord = word.replace(/[^a-zA-Z]/g, ''); // Remove non-alphabetic characters
-    if (trimmedWord.length > 0) {
-      if (isWordValid(trimmedWord)) {
-        result.push(trimmedWord);
-      } else {
-        result.push(`[ ${trimmedWord} ]`);
-      }
-    } else {
-      result.push(word); // Preserve non-alphabetic characters as is
-    }
+async function fetchDictionary(filePath) {
+  try {
+    const response = await axios.get(filePath);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching dictionary:', error.message);
+    return '';
   }
-
-  return result.join(' ');
 }
 
-// Example usage
-const inputText = "I like appple and bananana."; // "apple" and "banana" are misspelled.
-const checkedText = spellCheck(inputText);
-console.log(checkedText);
+(async () => {
+  const dictionaryFilePath = 'https://raw.githubusercontent.com/dwyl/english-words/master/words.txt';
+
+  try {
+    // Fetch the dictionary data from the URL
+    const dictionaryData = await fetchDictionary(dictionaryFilePath);
+
+    // Load the dictionary and perform spell-checking
+    const dictionary = loadDictionary(dictionaryData);
+
+    function isWordValid(word) {
+      return dictionary.includes(word.toLowerCase());
+    }
+
+    function spellCheck(text) {
+      const words = text.split(/\s+/);
+      const result = [];
+
+      for (const word of words) {
+        const trimmedWord = word.replace(/[^a-zA-Z]/g, ''); // Remove non-alphabetic characters
+        if (trimmedWord.length > 0) {
+          if (isWordValid(trimmedWord)) {
+            result.push(trimmedWord);
+          } else {
+            result.push(`[ ${trimmedWord} ]`);
+          }
+        } else {
+          result.push(word); // Preserve non-alphabetic characters as is
+        }
+      }
+
+      return result.join(' ');
+    }
+
+    // Example usage
+    const inputText = "I like appple and bananana."; // "apple" and "banana" are misspelled.
+    const checkedText = spellCheck(inputText);
+    console.log(checkedText);
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+})();
