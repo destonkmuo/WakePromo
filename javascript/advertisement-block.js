6b887f8d4fc47107713b734404ada7c96dab4bcf
destonkmuo <destonmuo.contact@gmail.com>
2023-10-05 18:53:40 -0500

    Update to skip unskippable ads

--- javascript/advertisement-block.js
+++ javascript/advertisement-block.js
@@ -2,8 +2,12 @@ function blockAdsOnNewVideo() {
       // Move the setInterval inside the callback to ensure the result is available
       setInterval(function() {
         var skipButton = document.getElementsByClassName("ytp-ad-skip-button");
-        if (skipButton != undefined && skipButton.length > 0) {
+        var video = document.getElementsByClassName("video-stream html5-main-video");
+        var adShowing = document.getElementsByClassName("ad-showing");
+        if (adShowing[0] != undefined) {
+          video[0].currentTime = video[0].duration;
           skipButton[0].click();
+          console.log("skipped ad");
         }
       }, 1);
   }
